97c1d3df17c97eafdff93b58b2f52883e58a6aa2
Owen <117956892+Owen756@users.noreply.github.com>
2024-05-14 18:55:34 -0700

    Made a method to get look ahead pose

--- src/main/java/frc/robot/util/FloorShotUtil.java
+++ src/main/java/frc/robot/util/FloorShotUtil.java
@@ -4,18 +4,45 @@
 
 package frc.robot.util;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.localization.LocalizationSubsystem;
 import frc.robot.swerve.SwerveSubsystem;
+import frc.robot.vision.VisionSubsystem;
 
 public class FloorShotUtil {
   public final SwerveSubsystem swerve;
+  public final LocalizationSubsystem localization;
+  public final VisionSubsystem vision;
 
-  public FloorShotUtil(SwerveSubsystem swerve) {
+  private final double TimeConstant = 0.02;
+
+  public FloorShotUtil(SwerveSubsystem swerve, LocalizationSubsystem localization, VisionSubsystem vision) {
     this.swerve = swerve;
+    this.localization = localization;
+    this.vision = vision;
   }
 
-  public Translation2d chassisSpeedsToVector() {
+  public Transform2d getRobotVector() {
     var chassisSpeeds = swerve.getFieldRelativeSpeeds();
-    return new Translation2d(chassisSpeeds.vxMetersPerSecond, chassisSpeeds.vyMetersPerSecond);
+    return new Transform2d(new Translation2d(chassisSpeeds.vxMetersPerSecond, chassisSpeeds.vyMetersPerSecond), new Rotation2d(chassisSpeeds.omegaRadiansPerSecond));
+  }
+
+  public Pose2d getLookAheadPose() {
+    var chassisSpeeds = getRobotVector();
+    Pose2d currentPose = localization.getPose();
+    double lookAheadX = currentPose.getX() + (chassisSpeeds.getX() * TimeConstant);
+    double lookAheadY = currentPose.getY() + (chassisSpeeds.getY() * TimeConstant);
+    double lookAheadOmega = currentPose.getRotation().getRadians() + (chassisSpeeds.getRotation().getRadians() * TimeConstant);
+
+    return new Pose2d(lookAheadX, lookAheadY, new Rotation2d(lookAheadOmega));
+  }
+
+  public Translation2d getLookAheadNoteVector() {
+    double lookAheadDistanceToFloorSpot = vision.getDistanceAngleFloorShot().distance();
+
+    return new Translation2d();
   }
 }
