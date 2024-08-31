4bbbaab8a90ca8c8ef7dff3807fbb10739e0219d
Simon <87281874+simonp17@users.noreply.github.com>
2024-08-31 12:56:45 -0700

    Tune interpolated vision points and math

--- src/main/java/frc/robot/vision/VisionUtil.java
+++ src/main/java/frc/robot/vision/VisionUtil.java
@@ -4,56 +4,70 @@
 
 package frc.robot.vision;
 
+import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import java.util.List;
 
+// 260.55cm
+
+// 13.9735
+
+// 2.62 meters
+
 public class VisionUtil {
   private static final VisionInterpolationData SUBWOOFER =
       new VisionInterpolationData(
-          new Pose2d(15.279, 5.522, new Rotation2d(0)),
-          new Pose2d(15.289, 5.587, new Rotation2d(0)),
-          "Subwoofer");
-  private static final VisionInterpolationData WING_LINE_AMP =
+          // output before changing math, with just subwoofer point:   15.162,     5.749
+          // output after changing math, with just subwoofer point: 15.222, 5.524
+          new Pose2d(15.2245, 5.522, new Rotation2d(0)),
+          new Pose2d(15.194, 5.634, new Rotation2d(0)),
+          "SUBWOOFER");
+  private static final VisionInterpolationData PODIUM_SPEAKER_INTERSECTION =
+      // output with both subwoofer and intersection: 12.962, 5.623
+      // output with only intersection, after redoing math:
       new VisionInterpolationData(
-          new Pose2d(11.232, 7.78, new Rotation2d(0)),
-          new Pose2d(11.4, 7.865, new Rotation2d(0)),
-          "WING_LINE_AMP");
-
-  private static final VisionInterpolationData STAGE_FRONT =
+          new Pose2d(13.0745, 5.522, new Rotation2d(0)),
+          new Pose2d(13.125, 5.722, new Rotation2d(0)),
+          "PODIUM_SPEAKER_INTERSECTION");
+  private static final VisionInterpolationData WING_LINE_MIDDLE =
+      // output with both subwoofer and intersection: 12.962, 5.623
+      // output with only intersection, after redoing math:
       new VisionInterpolationData(
-          new Pose2d(12.85, 4.71, new Rotation2d(0)),
-          new Pose2d(12.83, 4.66, new Rotation2d(0)),
-          "StageFront");
-  private static final VisionInterpolationData STAGE_MIDDLE =
-      new VisionInterpolationData(
-          new Pose2d(11.98, 5.16, new Rotation2d(0)),
-          new Pose2d(12.0, 5.11, new Rotation2d(0)),
-          "StageMiddle");
-
+          new Pose2d(11.059, 6.842, new Rotation2d(0)),
+          new Pose2d(11.16, 6.845, new Rotation2d(0)),
+          "WING_LINE_MIDDLE");
+  private static final double SENSITIVITY = 2;
   private static final List<VisionInterpolationData> DATA_POINTS =
-      List.of(SUBWOOFER, WING_LINE_AMP);
+      List.of(SUBWOOFER, PODIUM_SPEAKER_INTERSECTION, WING_LINE_MIDDLE);
 
   /**
    * @param visionInput - pose from the limelight
    * @return a transformed pose that can be added to the pose estimator
    */
   public static Pose2d interpolatePose(Pose2d visionInput) {
-    double distanceSum = 0;
+    DogLog.log("Debug/IntersectionMeasuredPose", PODIUM_SPEAKER_INTERSECTION.measuredPose());
+
+    double unnormalizedWeightsSum = 0;
     for (var dataPoint : DATA_POINTS) {
       double distancePoint =
           dataPoint.visionPose().getTranslation().getDistance(visionInput.getTranslation());
 
-      distanceSum += distancePoint;
+      var weight = 1 / Math.pow(distancePoint, SENSITIVITY);
+
+      unnormalizedWeightsSum += weight;
     }
     double weightedX = 0;
     double weightedY = 0;
 
+
+
     for (var dataPoint : DATA_POINTS) {
       double distancePoint =
           dataPoint.visionPose().getTranslation().getDistance(visionInput.getTranslation());
-      var weight = 1 - ((distanceSum - distancePoint) / distanceSum);
-      var result = dataPoint.visionPose().minus(dataPoint.measuredPose()).times(weight);
+         var weight = (1 / Math.pow(distancePoint, SENSITIVITY)) / unnormalizedWeightsSum;
+      DogLog.log("Debug/" + dataPoint.label() + "/Weight", weight);
+      var result = dataPoint.measuredPose().minus(dataPoint.visionPose()).times(weight);
       weightedX += result.getX();
       weightedY += result.getY();
     }
