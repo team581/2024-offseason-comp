2d0ecdb20e6d7b9697879244071e77fdcebe36ae
Jonah Snider <jonah@jonahsnider.com>
2024-09-07 11:22:53 -0700

    Add a method to get points of a bounding box

--- src/main/java/frc/robot/auto_manager/BoundingBox.java
+++ src/main/java/frc/robot/auto_manager/BoundingBox.java
@@ -4,15 +4,25 @@
 
 package frc.robot.auto_manager;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import java.util.List;
 import org.apache.commons.geometry.euclidean.twod.Bounds2D;
 import org.apache.commons.geometry.euclidean.twod.Vector2D;
 import org.apache.commons.geometry.euclidean.twod.path.LinePath;
 import org.apache.commons.numbers.core.Precision;
+import org.apache.commons.numbers.core.Precision.DoubleEquivalence;
 
 public class BoundingBox {
+  private static final DoubleEquivalence PRECISION = Precision.doubleEquivalenceOfEpsilon(0.01);
+
   private static Vector2D translationToVector(Translation2d translation) {
     return Vector2D.of(translation.getX(), translation.getY());
+  }
+
+  private static Translation2d vectorToTranslation(Vector2D vector) {
+    return new Translation2d(vector.getX(), vector.getY());
   }
 
   private final Bounds2D bounds;
@@ -23,7 +33,7 @@ public class BoundingBox {
       Translation2d bottomLeft,
       Translation2d bottomRight) {
     bounds =
-        LinePath.builder(Precision.doubleEquivalenceOfEpsilon(0.01))
+        LinePath.builder(PRECISION)
             .appendVertices(
                 translationToVector(topLeft),
                 translationToVector(topRight),
@@ -35,5 +45,11 @@ public class BoundingBox {
 
   public boolean contains(Translation2d translation) {
     return bounds.contains(translationToVector(translation));
+  }
+
+  public List<Pose2d> getPoints() {
+    return bounds.toRegion(PRECISION).getVertices().stream()
+        .map(vertex -> new Pose2d(vectorToTranslation(vertex), new Rotation2d(0)))
+        .toList();
   }
 }
