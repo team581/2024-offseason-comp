e9a0d36b8a47f8d33f3fc44c17459f64e13a8340
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-09-26 16:42:46 -0700

    add new auto to pathplanner and auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -15,6 +15,7 @@ public enum AutoSelection {
   // CHOREO_DROP_8_6("Red Drop 8 to 7", "Blue Drop 8 to 7"),
   // CHOREO_FOUR_PIECE_2_5("Red 4 Piece 2 to 5", "Blue 4 Piece 2 to 5"),
   // CHOREO_THREE_PIECE("Red 3 Piece 8 to 6", "Blue 3 Piece 8 to 6"),
+  CHOREO_3_PIECE_DROP("Red 3 Piece 8 to 7 + 10", "Blue 3 Piece 8 to 7 + 10"),
   CHOREO_SIX_PIECE("Red Six Piece 2 to 4", "Blue Six Piece 2 to 4"),
   CHOREO_OP("Red OP", "Blue OP"),
   CHOREO_FOUR_PIECE_2_3("Red 4 Piece 2 to 3", "Blue 4 Piece 2 to 3"),
