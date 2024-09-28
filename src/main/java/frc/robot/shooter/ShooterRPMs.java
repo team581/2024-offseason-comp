73a1c0a6d4ceaebaa4d1e43baeb6fb739b587eec
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-09-28 12:45:44 -0700

    Adjust shooter spin ratio from 3.5:5 to 4:5

--- src/main/java/frc/robot/shooter/ShooterRPMs.java
+++ src/main/java/frc/robot/shooter/ShooterRPMs.java
@@ -20,7 +20,7 @@ public class ShooterRPMs {
   public static final double AMP_TOLERANCE = 100;
   public static final double TOLERANCE = 125;
   public static final double FEEDING_TOLERANCE = 250;
-  public static final double SPIN_RATIO = 3.5 / 5.0;
+  public static final double SPIN_RATIO = 4.0 / 5.0;
 
   private ShooterRPMs() {}
 }
