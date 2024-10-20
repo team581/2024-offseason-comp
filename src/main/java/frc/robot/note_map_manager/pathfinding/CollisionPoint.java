e8efb30b309d14ed55d533115b4ef6dc1ac03a1a
Jonah Snider <jonah@jonahsnider.com>
2024-10-20 00:07:31 -0700

    Integrate heuristic pathfinder with note map and add logging

--- src/main/java/frc/robot/note_map_manager/pathfinding/CollisionPoint.java
+++ src/main/java/frc/robot/note_map_manager/pathfinding/CollisionPoint.java
@@ -2,8 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.auto_manager.pathfinding;
+package frc.robot.note_map_manager.pathfinding;
 
 import edu.wpi.first.math.geometry.Translation2d;
 
-record CollisionPoint(Translation2d point, double radius) {}
+record CollisionPoint(String label, Translation2d point, double radius) {}
