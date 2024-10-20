340dc1db64c473cae5a471e0e629b5f7f08ff78d
Jonah Snider <jonah@jonahsnider.com>
2024-10-19 23:11:19 -0700

    Add IDX config

---
+++ .idx/dev.nix
@@ -0,0 +1,48 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.jdk17
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "redhat.java"
+      "vscjava.vscode-java-debug"
+      "vscjava.vscode-java-dependency"
+      "vscjava.vscode-java-pack"
+      "vscjava.vscode-java-test"
+      "vscjava.vscode-maven"
+      "vscjava.vscode-gradle"
+      "wpilibsuite.vscode-wpilib"
+      "EditorConfig.EditorConfig"
+      "ms-python.black-formatter"
+      "ms-python.debugpy"
+      "ms-python.python"
+    ];
+    # Enable previews
+    previews = {
+      enable = false;
+      previews = {
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = 'npm install';
+      };
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
