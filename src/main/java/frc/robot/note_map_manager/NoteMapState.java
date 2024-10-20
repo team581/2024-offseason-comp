83e281691adba27056e823e83cf8b037d4d63df0
Jonah Snider <jonah@jonahsnider.com>
2024-10-19 23:23:13 -0700

    Rename auto_manager package to note_map_manager

--- src/main/java/frc/robot/note_map_manager/NoteMapState.java
+++ src/main/java/frc/robot/note_map_manager/NoteMapState.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.auto_manager;
+package frc.robot.note_map_manager;
 
 public enum NoteMapState {
   STOPPED,
