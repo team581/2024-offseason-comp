666969bb11be57bd0a4833959789e69b14e4ced1
Simon <87281874+simonp17@users.noreply.github.com>
2024-10-20 15:59:00 -0700

    Increase starting health of notes in note map to 20

--- src/main/java/frc/robot/note_tracking/NoteMapElement.java
+++ src/main/java/frc/robot/note_tracking/NoteMapElement.java
@@ -8,6 +8,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public record NoteMapElement(double expiresAt, Translation2d noteTranslation, int health) {
   public NoteMapElement(double expiry, Translation2d translation) {
-    this(expiry, translation, 5);
+    this(expiry, translation, 20);
   }
 }
