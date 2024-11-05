d4e2782ea111223ec9a600984f4535fb8a7a7fee
Simon <87281874+simonp17@users.noreply.github.com>
2024-11-05 14:37:26 -0800

    Fix auto selection for planned autos at madtown

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -6,10 +6,9 @@ package frc.robot.autos;
 
 public enum AutoSelection {
   DO_NOTHING("", ""),
-  OP("Red OP", ""),
-  RED_AMP_OP_NM("Red Amp OP NM", ""),
-  RED_MID_DROP_RACE_NM("Red Mid Drop Race NM", ""),
-  RED_SOURCE_SIDE_RACE_NM("Red Source Side Race NM", "");
+  OP("Red OP", "Blue OP"),
+  RED_AMP_OP_NM("Red Amp OP NM", "Blue Amp OP NM"),
+  RED_SOURCE_SIDE_RACE_NM("Red Source Side Race NM", "Blue Source Side Race NM");
 
   public final String redAutoName;
   public final String blueAutoName;
