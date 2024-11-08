d4ce21efd9c2064ccf52dacb9938aebd9f060e57
Simon <87281874+simonp17@users.noreply.github.com>
2024-11-07 17:07:40 -0800

    Edit auto selection names

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -6,9 +6,9 @@ package frc.robot.autos;
 
 public enum AutoSelection {
   DO_NOTHING("", ""),
-  OP("Red OP", "Blue OP"),
-  RED_AMP_OP_NM("Red Amp OP NM", "Blue Amp OP NM"),
-  RED_SOURCE_SIDE_RACE_NM("Red Source Side Race NM", "Blue Source Side Race NM");
+  OP_NON_NM("Red OP", "Blue OP"),
+  AMP_SIDE_NM("Red Amp OP NM", "Blue Amp OP NM"),
+  SOURCE_SIDE_NM("Red Source Side Race NM", "Blue Source Side Race NM");
 
   public final String redAutoName;
   public final String blueAutoName;
